321a92c53ce1b1dd4506130b014285fc37524f05
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-10-11 14:11:13 +0200

    Update get_gnos_donor.sh

--- bin/get_gnos_donor.sh
+++ bin/get_gnos_donor.sh
@@ -20,13 +20,13 @@ mkdir -p $donor_dir
 echo "Downloading tumor data for $donor. Tumor: $tumor - Normal: $normal"
 
 echo "Downloading tumor BAM: $tumor"
-#(cd $tumor_tmp; $gnos_client https://gtrepo-ebi.annailabs.com/cghub/data/analysis/download/$tumor)
+(cd $tumor_tmp; $gnos_client https://gtrepo-ebi.annailabs.com/cghub/data/analysis/download/$tumor)
 mv $tumor_tmp/*/*.bam "$donor_dir/tumor.bam"
 mv $tumor_tmp/*/*.bam.bai "$donor_dir/tumor.bam.bai"
 mv $tumor_tmp/*/*.bam.bas "$donor_dir/tumor.bam.bas"
 
 echo "Downloading normal BAM: $normal"
-#(cd $normal_tmp; $gnos_client https://gtrepo-ebi.annailabs.com/cghub/data/analysis/download/$normal)
+(cd $normal_tmp; $gnos_client https://gtrepo-ebi.annailabs.com/cghub/data/analysis/download/$normal)
 mv $normal_tmp/*/*.bam "$donor_dir/normal.bam"
 mv $normal_tmp/*/*.bam.bai "$donor_dir/normal.bam.bai"
 mv $normal_tmp/*/*.bam.bas "$donor_dir/normal.bam.bas"
