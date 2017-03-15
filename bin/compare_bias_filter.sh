008130db7fce2919af32abfacfa89e70551ae3f3
Miguel Vazquez <miguel.vazquez@cnio.es>
2017-03-15 10:38:07 +0000

    Fixed comparison of bias filter

--- bin/compare_bias_filter.sh
+++ bin/compare_bias_filter.sh
@@ -12,8 +12,27 @@ tmp_dir="$base_dir/tmp/Bias_Filter_compare"
 
 mkdir -p $tmp_dir
 
-grep OXOG $original_vcf | cut -f 1,2,4,5 | sort > $tmp_dir/${donor}.orig
-grep OXOG $new_vcf | cut -f 1,2,4,5 | sort > $tmp_dir/${donor}.new
+for tag in bPcr bSeq; do
+        new_list=$tmp_dir/${donor}.$tag.new
+        orig_list=$tmp_dir/${donor}.$tag.orig
 
-echo "Differences in OXOG filtered variants"
-diff $tmp_dir/${donor}.orig $tmp_dir/${donor}.new
+	grep $tag $original_vcf | grep -v "^#" | cut -f 1,2,4,5 | sort > $orig_list
+	grep $tag $new_vcf | grep -v "^#" | cut -f 1,2,4,5 | sort > $new_list
+
+	extra_lines_c=$(comm -2 -3 $new_list $orig_list | wc -l)
+	common_lines_c=$(comm -1 -2 $new_list $orig_list | wc -l)
+	missing_lines_c=$(comm -1 -3 $new_list $orig_list | wc -l)
+	extra_lines_ex=$(comm -2 -3 $new_list $orig_list | head -n 3| tr '\t' ':' | tr '\n' ','|sed 's/,$//')
+	missing_lines_ex=$(comm -1 -3 $new_list $orig_list | head -n 3| tr '\t' ':' | tr '\n' ','|sed 's/,$//')
+
+	echo
+	echo "Comparison for $donor tag $tag"
+	echo "---"
+	echo "Common: $common_lines_c"
+	echo "Extra: $extra_lines_c"
+	[[ $extra_lines_c -gt 0 ]] && echo "    - Example: $extra_lines_ex"
+	echo "Missing: $missing_lines_c"
+	[[ $missing_lines_c -gt 0 ]] && echo "    - Example: $missing_lines_ex"
+	echo
+done
+
