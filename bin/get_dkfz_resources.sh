d96cc4bc0efc188acc74a9f449d652b2b8e96fed
Miguel Vazquez <miguel.vazquez@cnio.es>
2017-07-10 07:35:40 +0000

    Download some resources and use them locally in Delly

--- bin/get_dkfz_resources.sh
+++ bin/get_dkfz_resources.sh
@@ -10,3 +10,5 @@ gnos_client="gtdownload -c $base_dir/etc/keyfile.txt"
 
 (cd $tmp_dir; $gnos_client -vv https://gtrepo-dkfz.annailabs.com/cghub/data/analysis/download/32749c9f-d8aa-4ff5-b32c-296976aec706)
 mv $tmp_dir/*/*.tar.gz $resource_dir
+
+wget "https://dcc.icgc.org/api/v1/download?fn=/PCAWG/reference_data/pcawg-delly/hs37d5_1000GP.gc" -O $resource_dir/hs37d5_1000GP.gc
