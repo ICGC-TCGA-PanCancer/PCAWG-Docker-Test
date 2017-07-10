70d2e133bb4baae1da275dcdf517f209361cd4ac
Miguel Vazquez <miguel.vazquez@cnio.es>
2017-07-10 07:40:28 +0000

    Change filename of script

--- bin/run_workflow.sh
+++ bin/run_workflow.sh
@@ -40,7 +40,7 @@ function get_unaligned_bams {
 	local donor=$1
 	echo "Downloading un-aligned BAM files for $donor"
 	if [ ! -f $base_dir/data/$donor/normal_unaligned_bams ]; then 
-           	$base_dir/bin/download_unaligned.sh $donor
+           	$base_dir/bin/get_unaligned_bams.sh $donor
 	fi
 
 	if [ $? == 0 ]; then
